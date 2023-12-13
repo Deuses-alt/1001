8da456f602987ed09ce665cd977355dac5ed19dd
Deuses-alt <133817633+Deuses-alt@users.noreply.github.com>
2023-12-13 16:43:34 +0300

    Add files via upload

---
+++ service.py
@@ -0,0 +1,109 @@
+from random import randint
+from time import sleep
+from data import *
+
+def fight(current_enemy):
+    round = randint(1, 2)
+    enemy = enemies[current_enemy]
+    enemy_hp = enemies[current_enemy]['hp']
+    print(f'Противник - {enemy["name"]}: {enemy["script"]}')
+    input('Enter чтобы продолжить')
+    print()
+    while player['hp'] > 0 and enemy_hp > 0:
+        if round % 2 == 1:
+            print(f'{player["name"]} атакует {enemy["name"]}.')
+            crit = randint(1, 100)
+            if crit < player['luck']:
+                enemy_hp -= player['attack'] * 3
+            else:
+                enemy_hp -= player['attack']
+            sleep(1)
+        else:
+            print(f'{enemy["name"]} атакует {player["name"]}.')
+            player['hp'] -= enemy['attack'] * player['armor']
+            sleep(1)
+        print(f'''{player['name']}: {player['hp']}
+{enemy['name']}: {enemy_hp}''')
+        print()
+        sleep(1)
+        round += 1
+
+    if player['hp'] > 0:
+        print(f'Противник - {enemy["name"]}: {enemy["win"]}')
+        current_enemy += 1
+    else:
+        print(f'Противник - {enemy["name"]}: {enemy["loss"]}')
+    player['hp'] = 100
+    return current_enemy
+
+
+def training(training_type):
+    for i in range(0, 101, 20):
+        print(f'Тренировка завершена на {i}%')
+        sleep(1.5)
+    if training_type == '1':
+        player['attack'] += 2
+        print(f'Тренировка окончена! Теперь ваша величина атаки равна {player["attack"]}')
+    elif training_type == '2':
+        player['armor'] -= .09
+        print(f'Тренировка окончена! Теперь броня поглощает {100 - player["armor"] * 100}% урона')
+    print()
+
+
+
+def display_player():
+    print(f'Игрок - {player["name"]}')
+    print(f'Величина атаки - {player["attack"]}. Шанс критического урона ({player["attack"]}ед.) равен {player["luck"]}')
+    print(f'Броня поглощает {(1 - player["armor"]) * 100}% урона')
+
+
+def display_enemy(current_enemy):
+    enemy = enemies[current_enemy]
+    print(f'Противник - {enemy["name"]}')
+    print(f'Веилична атаки - {enemy["attack"]}')
+    print(f'Здоровье - {enemy["hp"]}')
+
+
+def dispay_inventory():
+    print(f'У меня есть:')
+    for value in player['inventory']:
+        print(value)
+    if 'Зелье удачи' in player['inventory']:
+        potion = input('''Выпить зелье:
+                       1 - Da
+                       2 - Net''')
+        if potion == '1':
+            player['luck'] += 7
+            player['inventory'].remove('Зелье удачи')
+
+
+def shop():
+    print(f'Привет, ваша фраза')
+    print(f'Деньги: {player["money"]}')
+    for key, value in items.items():
+        print(f'{key} - {value["name"]}: {value["price"]}')
+    item = input()
+    if item in player['inventory']:
+        print(f'Уже есть')
+    elif player['money'] >= items[item]['price']:
+        print(f'Купил')
+        player['inventory'].append(items[item]["name"])
+        player['money'] -=  items[item]['price']
+    else:
+        print('Иди работай')
+    print()
+    print('Фраза прощания с игроком')
+    print()
+
+def earn():
+    print(f'Добро пожаловать на завод!')
+    result = randint(1, 100)
+    sleep(1.5)
+    print('Иииии....')
+    sleep(1.5)
+    if result < 67:
+        print('+500')
+        player['money'] += 500
+    else:
+        print('-500')
+        player['money'] -= 500
